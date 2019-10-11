de77d9ef816aaa84d8a60a8c5f3effb81f5d455c
Martin Grigorov <m.grigorov@student.fontys.nl>
2019-10-11 12:02:13 +0200

    DEtails page created

---
+++ Averi/Averi/DetailsTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsTableViewController.swift
+//  Averi
+//
+//  Created by Student on 11/10/2019.
+//  Copyright © 2019 Dimitar Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
