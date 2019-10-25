0586d591657144ce83ca38351a2d3a6d48af3c6d
Martin Grigorov <m.grigorov@student.fontys.nl>
2019-10-25 14:40:17 +0200

    cancel and done added to create page

--- Averi/Averi/CreateTableViewController.swift
+++ Averi/Averi/CreateTableViewController.swift
@@ -16,56 +16,41 @@ class CreateTableViewController: UITableViewController {
     @IBOutlet weak var EntryLimitTextField: UITextField!
     @IBOutlet weak var EntryCostTextField: UITextField!
     @IBOutlet weak var DetailsTextField: UITextField!
-<<<<<<< HEAD
     
+    var event : Event?
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
+//        locationManager.delegate = self
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.requestWhenInUseAuthorization()
+//        locationManager.requestLocation()
         
-=======
-    var event : Event?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let name = NameTextField.text,
-            let location = LocationTextField.text,
-            let details = DetailsTextField.text{
-            event = Event.init(name: name, location: location, date: "14.06.2020", time: "14:30", entryLimit: 9, entryCost: 9.50, bio: details, photo: #imageLiteral(resourceName: "Party"))
-        }
->>>>>>> fe546d443995708312d3603f24553ee62aa5b5ce
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            
+            if segue.identifier == "SavePlayerDetail",
+                let name = NameTextField.text,
+                let location = LocationTextField.text,
+                let details = DetailsTextField.text {
+                event = Event.init(name: name, location: location, date: "14.06.2020", time: "14:30", entryLimit: 9, entryCost: 9.50, bio: details, photo: #imageLiteral(resourceName: "Party"))
+            }
+//        if let name = NameTextField.text,
+//            let location = LocationTextField.text,
+//            let details = DetailsTextField.text{
+//            event = Event.init(name: name, location: location, date: "14.06.2020", time: "14:30", entryLimit: 9, entryCost: 9.50, bio: details, photo: #imageLiteral(resourceName: "Party"))
+//        }
     }
-    
+
 }
-
-
-extension CreateTableViewController : CLLocationManagerDelegate {
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse {
-            locationManager.requestLocation()
-        }
-    }
+extension CreateTableViewController {
     
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            print("location:: \(location)")
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            NameTextField.becomeFirstResponder()
         }
-    }
-    
-    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        print("error:: \(error)")
-    }
+}
 }
