5222bf3b32a995a647fd8b50c49e21ebc745f0bd
Martin Grigorov <m.grigorov@student.fontys.nl>
2019-10-31 10:59:57 +0100

    Location class

--- Averi/Averi/LocationViewController.swift
+++ Averi/Averi/LocationViewController.swift
@@ -7,24 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
 class LocationViewController: UIViewController {
-
+    @IBOutlet weak var SearchBar: UISearchBar!
+    @IBOutlet weak var MapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension LocationViewController : CLLocationManagerDelegate {
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
     }
-    */
 
+    private func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.first != nil {
+            print("location:: (location)")
+        }
+    }
+
+    private func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: (error)")
+    }
 }
