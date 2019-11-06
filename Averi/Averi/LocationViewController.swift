e45087bb0d48eec20f58f3d99ff36305743cdeda
Martin Grigorov <m.grigorov@student.fontys.nl>
2019-11-06 15:04:42 +0100

    Search bar for location started

--- Averi/Averi/LocationViewController.swift
+++ Averi/Averi/LocationViewController.swift
@@ -51,7 +51,7 @@ extension LocationViewController : CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last{
             let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
             self.mapView.setRegion(region, animated: true)
         }
     }
